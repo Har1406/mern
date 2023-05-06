6b8e74a745e27fc5124e7b1678d09a9f0252ff8c
Harsh codage <127947433+harshpatel1406@users.noreply.github.com>
2023-05-06 16:59:29 +0530

    first

---
+++ server/client/build/precache-manifest.c08255511f160d6517901b26aa00acf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "886ff9312f1a4763e6e07a6c76150c8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ecd7b29329e42d11547",
+    "url": "/static/css/main.d3f13e23.chunk.css"
+  },
+  {
+    "revision": "2b34308db03ad2d00ab4",
+    "url": "/static/js/2.c35773ff.chunk.js"
+  },
+  {
+    "revision": "038db422c0414d8dfd31541a1bf2a15e",
+    "url": "/static/js/2.c35773ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ecd7b29329e42d11547",
+    "url": "/static/js/main.863b555a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.863b555a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
